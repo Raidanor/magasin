46010ecce25eda862a6a1e1dee68fe247d77f8e6
Raidanor <silverstallion30@gmail.com>
2025-02-02 09:54:47 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 5001
+
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, () => {
+    console.log("App is running on port http://localhost:" + PORT)
+})
