2ae36262550db2b017122fa4e5734d49522a8ef8
Raidanor <silverstallion30@gmail.com>
2025-02-04 20:11:02 -0400

    second commit

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,21 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
+import { connectDB } from "./lib/db.js"
 
 dotenv.config()
 
 const app = express()
+app.use(express.json())
 
 const PORT = process.env.PORT || 5001
 
 app.use("/api/auth", authRoutes)
 
 
+
 app.listen(PORT, () => {
     console.log("App is running on port http://localhost:" + PORT)
+
+    connectDB()
 })
