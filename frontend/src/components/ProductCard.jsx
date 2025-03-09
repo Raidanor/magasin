4c8fef71fc60772af482e4db809d136283e855ab
Raidanor <silverstallion30@gmail.com>
2025-03-08 22:56:41 -0400

    13th commit

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,6 +2,7 @@ import toast from "react-hot-toast";
 import { ShoppingCart } from "lucide-react";
 import { useUserStore } from "../stores/useUserStore";
 import { useCartStore } from "../stores/useCartStore";
+import { useEffect, useState } from "react";
 
 const ProductCard = ({ product }) => {
 	const { user } = useUserStore();
@@ -19,48 +20,44 @@ const ProductCard = ({ product }) => {
 		}
 	};
 
-    function testFunction()
-    {
-        console.log(product.sizes[0])
-    }
-
 	return (
-		<div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
-			<div className='relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl'>
-				<img className='object-cover w-full' src={product.image} alt='product image' />
-				{/* <div className='absolute inset-0 bg-black bg-opacity-20' /> */}
-			</div>
+        
+            <div className='flex w-full relative flex-col overflow-hidden rounded-lg border border-gray-700 shadow-lg'>
+                <div className='relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl'>
+                    <img className='object-cover w-full' src={product.image} alt='product image' />
+                    {/* <div className='absolute inset-0 bg-black bg-opacity-20' /> */}
+                </div>
 
-			<div className='mt-4 px-5 pb-5'>
-				<h5 className='text-xl font-semibold tracking-tight text-white'>{product.name}</h5>
-				<div className='mt-2 mb-5 flex items-center justify-between'>
-					<p>
-						<span className='text-3xl font-bold text-emerald-400'>${product.price}</span>
-					</p>
-				</div>
-                
-				<button
-					className='flex items-center justify-center rounded-lg bg-emerald-600 px-5 py-2.5 text-center text-sm font-medium
-					 text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300'
-					onClick={handleAddToCart}
-				>
-					<ShoppingCart size={22} className='mr-2' />
-					Add to cart
-				</button>
-                { product.sizes.length > 0 &&
-                    <ul className="">
-                        {product.sizes.map(size => (
-                            <li className="flex items-center justify-center rounded-lg px-5 py-2.5 text-center text-sm font-medium
-					 text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300">{size}</li>
-                        ))}
-                    </ul>
-                }
+                <div className='mt-4 px-5 pb-5'>
+                    <h5 className='text-xl font-semibold tracking-tight text-white'>{product.name}</h5>
+                    <div className='mt-2 mb-5 flex items-center justify-between'>
+                        <p>
+                            <span className='text-3xl font-bold text-emerald-400'>Rs.{product.price}</span>
+                        </p>
+                    </div>
+                    <div>
+                        <button
+                            className='flex items-center justify-center rounded-lg bg-emerald-600 px-5 py-2.5 text-center text-sm font-medium
+                            text-white hover:bg-emerald-700 focus:outline-none focus:ring-4 focus:ring-emerald-300'
+                            onClick={handleAddToCart}
+                        >
+                            <ShoppingCart size={22} className='mr-2' />
+                            Add to cart
+                        </button>
 
-                {/* <button className='rounded-lg' onClick={testFunction}>
-                    TEST
-                </button> */}
-			</div>
-		</div>
+                        
+                            { product.sizes.length > 0 &&
+                                <select className="flex bg-emerald-600 rounded-lg mt-2 justify-items-end" >
+                                    {product.sizes.map(size => (
+                                        <option value={size}>{size}</option>
+                                    ))}
+                                </select>
+                            }
+                        
+                    </div>
+                </div>
+            </div>
+       
 	);
 };
 export default ProductCard;
